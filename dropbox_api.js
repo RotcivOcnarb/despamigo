b940613df05a78b0b660e97c58e0589ad78d4941
Rotciv <rotciv_ocnarb@hotmail.com>
2021-08-31 20:23:03 -0300

    aa

---
+++ dropbox_api.js
@@ -0,0 +1,76 @@
+const https = require("https");
+
+function getFile(filename, callback){
+	
+	let hostname = "content.dropboxapi.com";
+	let path = "/2/files/download";
+	
+	let opts = {
+		hostname: hostname,
+		path: path,
+		method: 'POST',
+		headers: {
+			"Authorization": "Bearer " + process.env.DRIVE_TOKEN,
+			"Dropbox-API-Arg": JSON.stringify({
+				"path": "/" + filename
+			})
+		}
+	};
+		
+	let req = https.request(opts, function(res){
+		res.setEncoding('utf8');
+		let dt = "";
+		res.on('data', (chunk) => {
+			dt += chunk;
+		});
+		res.on('end', () => {
+			callback(dt);
+		});
+	});
+	req.end();
+}
+
+function updateFile(filename, contents, callback){
+	let hostname = "content.dropboxapi.com";
+	let path = "/2/files/upload";
+	
+	let req = https.request({
+		hostname: hostname,
+		path: path,
+		method: 'POST',
+		headers: {
+			"Authorization": "Bearer " + process.env.DRIVE_TOKEN,
+			"Content-Type": "text/plain; charset=utf-8",
+			"Dropbox-API-Arg": JSON.stringify({
+				"path": "/" + filename,
+				"mode": "overwrite",
+				"mute": true,
+				"strict_conflict": false
+			})
+		}
+		
+	}, callback);
+	req.write(contents);
+	req.end();
+}
+
+function saveDatabase(databaseName, database){
+	updateFile(databaseName + ".json", JSON.stringify(database))
+}
+
+function getDatabase(databaseName, callback){
+	getFile(databaseName + ".json", (content) => callback(JSON.parse(content)));
+}
+
+
+
+module.exports = {
+	saveDatabase: saveDatabase,
+	loadDatabase: getDatabase,
+	getFile: getFile,
+	updateFile: updateFile
+}
+
+
+
+
--- index.js
+++ index.js
@@ -2,15 +2,85 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const https = require("https");
+const crypto = require ("crypto");
+const fs = require('fs');
+const dropbox = require("./dropbox_api");
+
+let idList = [];
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + '/public'));
+
+decrypt((ids) => { 
+	idList = ids;
+	console.log(JSON.stringify(ids, 2, 2));
+});
+
 
 app.get("/", (req, res) => {
-
-	res.render("home");
-
+	res.render("home", {
+		"ids": ids,
+	});
 });
+
+app.get("/updatelist", (req, res) => {
+	res.sendStatus(200);
+	console.log("Received updatelist request");
+	decrypt((ids) => { 
+		idList = ids;
+		console.log(JSON.stringify(ids, 2, 2));
+	});
+	
+});
+
+function decrypt(callback){
+	const algorithm = "aes-256-cbc"; 
+	let initVector = Buffer.alloc(16);
+	let Securitykey = Buffer.alloc(32);
+
+	dropbox.getFile("despamigo/key.bin", (contents) => {
+		console.log("key.bin fetched from dropbox");
+		var buffer = Buffer.from(contents, 'binary');
+		buffer.copy(initVector, 0, 0, 16);
+		buffer.copy(Securitykey, 0, 16, 16+32);
+		
+		dropbox.getFile("despamigo/output.txt", (contents) => {
+			console.log("output.txt fetched from dropbox");
+			
+			const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
+			let decryptedData = decipher.update(contents, "hex", "utf-8");
+			decryptedData += decipher.final("utf8");
+			
+			callback(JSON.parse(decryptedData));
+		});
+	})
+
+	// fs.open(key, 'r', function(status, fd) {
+		// if (status) {
+			// console.log(status.message);
+			// return;
+		// }
+		// var buffer = Buffer.alloc(16+32);
+		// fs.read(fd, buffer, 0, 16+32, 0, function(err, num) {
+			// buffer.copy(initVector, 0, 0, 16);
+			// buffer.copy(Securitykey, 0, 16, 16+32);
+
+			// fs.readFile(file, 'utf8' , (err, data) => {
+				// if (err) {
+					// console.error(err);
+					// return;
+				// }
+				// const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
+				// let decryptedData = decipher.update(data, "hex", "utf-8");
+				// decryptedData += decipher.final("utf8");
+				
+				// callback(JSON.parse(decryptedData));
+			
+			// })
+		// });
+	// });
+}
 
 app.listen(1337, () => {
 	console.log("server started on port " + 1337);
