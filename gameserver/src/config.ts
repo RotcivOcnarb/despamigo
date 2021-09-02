62031f3ceaf77baa0bb2ea1517c8ecbb7a77a4cc
Rotciv <rotciv_ocnarb@hotmail.com>
2021-09-02 18:07:34 -0300

    novo

--- gameserver/src/config.ts
+++ gameserver/src/config.ts
@@ -1,13 +1,28 @@
-import Phaser from 'phaser';
+import { BootScene } from './scenes/boot-scene';
+import { MainMenuScene } from './scenes/main-menu-scene';
+import { GameScene } from './scenes/game-scene';
 
-export default {
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'Asteroid',
+  url: 'https://github.com/digitsensitive/phaser3-typescript',
+  version: '2.0',
+  width: 800,
+  height: 600,
   type: Phaser.AUTO,
   parent: 'game',
-  backgroundColor: '#33A5E7',
-  scale: {
-    width: 800,
-    height: 600,
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+  scene: [BootScene, MainMenuScene, GameScene],
+  input: {
+    keyboard: true,
+    mouse: false,
+    touch: false,
+    gamepad: false
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  backgroundColor: '#000000',
+  render: { pixelArt: false, antialias: true }
 };
